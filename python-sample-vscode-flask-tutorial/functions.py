3b98fbb8a90b48f1bb8313b140ff2b26eeec1c6c
i-martello <martelloignacio@gmail.com>
2023-06-03 19:46:47 -0300

    cambios

---
+++ python-sample-vscode-flask-tutorial/functions.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from markupsafe import escape
+import pandas as pd
+from flask_cors import CORS
+import datetime
+
+def upload_excel():  
+  def excel(codigos):
+    file_excel = request.files['file']
+    df_excel = pd.read_excel(file_excel, skiprows=9)  
+    df_excel = df_excel.drop(df_excel.columns[[0,5,6,7,8]], axis=1)
+    fecha_actual = datetime.datetime.now().strftime("%Y-%m-%d")
+    outpath_path = f"precios_{fecha_actual}.xlsx"
+    df_excel["PRECIO CON IVA"] =  [round(x) for x in df_excel["PRECIO CON IVA"].to_list()]
+    filas_filtradas = df_excel[df_excel["CÓDIGO"].isin(codigos)]
+    columnas = [round(x) for x in filas_filtradas["PRECIO CON IVA"].to_list()]
+    filas_filtradas.insert(3, "Precio", [round(x * 1.5) for x in columnas] )
+    return filas_filtradas, outpath_path  
+    # json =  filas_filtradas.to_json(orient='records')
+    # filas_filtradas.to_excel(outpath_path, index=False)
+
+  with open('./codigos.txt') as txt_file:
+    lineas_sin_limpiar = txt_file.readlines()
+    lineas_txt = [linea.strip() for linea in lineas_sin_limpiar]
+    json_excel = excel(lineas_txt)     
+  
+  return json_excel   
