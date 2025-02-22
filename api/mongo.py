a066e9c67e7c3d0e2438e2654c5bc952c9eb476e
i-martello <martelloignacio@gmail.com>
2025-02-22 18:01:35 -0300

    cambio

--- api/mongo.py
+++ api/mongo.py
@@ -1,10 +1,17 @@
 from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 MONGO_KEY = os.environ.get("MONGO_KEY")
 client = MongoClient(MONGO_KEY)
 
-db = client["Precios-CopyArt"]
-collection = db["Precios"]
+try:
+    # Verificamos la conexión listando las bases de datos
+    db = client["Precios-CopyArt"]
+    collection = db["Precios"]
+    print("Conexión exitosa a la base de datos")
+
+except Exception as e:
+    print("Error de conexión:", e)
